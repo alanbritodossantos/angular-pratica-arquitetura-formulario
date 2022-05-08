9ccb5fba3b6bb8222803e38e1cefa0007de14ce9
Alan <alanbritodossantos2015@gmail.com>
2022-05-08 02:34:06 -0300

    Formulário Template-Driven

--- gatitobook/src/app/home/home.module.ts
+++ gatitobook/src/app/home/home.module.ts
@@ -4,19 +4,11 @@ import { CommonModule } from '@angular/common';
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
 import { LoginComponent } from './login/login.component';
-
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [
-    HomeComponent,
-    LoginComponent
-  ],
-  imports: [
-    CommonModule,
-    HomeRoutingModule
-  ],
-  exports:[
-    HomeComponent
-  ],
+  declarations: [HomeComponent, LoginComponent],
+  imports: [CommonModule, HomeRoutingModule, FormsModule],
+  exports: [HomeComponent],
 })
-export class HomeModule { }
+export class HomeModule {}
