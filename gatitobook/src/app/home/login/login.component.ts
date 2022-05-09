9815dd4ce8c4cdf71c0e96e23c11d94275c9904c
Alan <alanbritodossantos2015@gmail.com>
2022-05-08 21:59:40 -0300

    Redirecionando a rota

--- gatitobook/src/app/home/login/login.component.ts
+++ gatitobook/src/app/home/login/login.component.ts
@@ -1,6 +1,6 @@
 import { AutenticacaoService } from './../../autenticacao/autenticacao.service';
 import { Component, OnInit } from '@angular/core';
-
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -11,14 +11,17 @@ export class LoginComponent implements OnInit {
   usuario = '';
   senha = '';
 
-  constructor(private authService: AutenticacaoService) {}
+  constructor(
+    private authService: AutenticacaoService,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {}
 
   login() {
     this.authService.autenticar(this.usuario, this.senha).subscribe(
       () => {
-        console.log('Autenticado com sucesso');
+        this.router.navigate(['animais']);
       },
 
       (error) => {
