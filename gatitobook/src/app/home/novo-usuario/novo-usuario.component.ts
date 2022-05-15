6a5ffcfa3bb24b8b9602033d68e720edfb111be6
Alan <alanbritodossantos2015@gmail.com>
2022-05-15 02:31:58 -0300

    Validações Assíncronas

--- gatitobook/src/app/home/novo-usuario/novo-usuario.component.ts
+++ gatitobook/src/app/home/novo-usuario/novo-usuario.component.ts
@@ -1,3 +1,4 @@
+import { UsuarioExisteService } from './usuario-existe.service';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { NovoUsuarioService } from './novo-usuario.service';
@@ -14,14 +15,15 @@ export class NovoUsuarioComponent implements OnInit {
 
   constructor(
     private formBuilder: FormBuilder,
-    private novoUsuarioService: NovoUsuarioService
+    private novoUsuarioService: NovoUsuarioService,
+    private UsuarioExisteService: UsuarioExisteService
   ) {}
 
   ngOnInit(): void {
     this.novoUsuarioForm = this.formBuilder.group({
-      email: ['',[Validators.required, Validators.email]],
-      fullName: ['',[Validators.required, Validators.minLength(4)]],
-      userName: ['', [minusculoValidator]],
+      email: ['', [Validators.required, Validators.email]],
+      fullName: ['', [Validators.required, Validators.minLength(4)]],
+      userName: ['', [minusculoValidator],[this.UsuarioExisteService.usuarioJaExiste()]],
       password: [''],
     });
   }
